9b4cf26a2e87eaed669d352ec67ad58dde94c68f
chaewon-45 <22300392@handong.ac.kr>
2026-01-05 17:10:56 +0900

    selectview change

--- Shoppingmate_Frontend/View/SelectView.swift
+++ Shoppingmate_Frontend/View/SelectView.swift
@@ -20,29 +20,30 @@ struct SelectView: View {
     var body: some View {
         NavigationStack {
             ZStack {
-                Color(red:249/255, green: 250/255, blue: 251/255)
+                Color(red: 0.98, green: 0.98, blue: 0.98)
                     .ignoresSafeArea(edges: .all)
-                VStack {
-                    HStack {
-                        VStack(alignment:.leading) {
-                            Text("서비스 이용 목적을 픽픽하세요!")
-                                .font(
-                                    Font.custom("Arial", size: 22)
-                                        .weight(.bold)
-                                )
-                                .foregroundColor(Color(red: 0.26, green: 0.22, blue: 0.79))
-                                .frame(width: 362, alignment: .topLeading)
-                                .padding(.bottom, 3)
-                            Text("서비스를 이용해 주셔서 감사합니다!")
-                                .foregroundColor(Color(red:106/255, green: 114/255, blue: 130/255))
-                                .font(.system(size: 15))
-                                .fontWeight(.regular)
-                        }
-                        .padding()
-                        Spacer()
-                    }
+                VStack(alignment: .leading) {
+                    Spacer()
+                    Image("PICPICK")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 147, height: 25)
+                        .clipped()
+                        .padding(.bottom, 25)
+                    Text("초간단 온/오프라인 가격비표")
+                      .font(Font.custom("Arial", size: 26))
+                      .multilineTextAlignment(.center)
+                      .foregroundColor(.black)
+                      .padding(.bottom, 15)
+                    Text("지금 바로,\n시작해 볼까요?")
+                      .font(
+                        Font.custom("Arial", size: 43)
+                          .weight(.bold)
+                      )
+                      .foregroundColor(.black)
+                      .padding(.bottom, 160)
                     VStack {
-                        //일반 사용자 버튼
+                        //게스트 로그인 버튼
                         HStack(alignment: .center, spacing: 8) {
                             ZStack {
                                 Text("게스트 로그인")
@@ -52,36 +53,6 @@ struct SelectView: View {
                                   )
                                   .multilineTextAlignment(.center)
                                   .foregroundColor(.white)
-    //                            RoundedRectangle(cornerRadius: 13)
-    //                                .fill(Color.white)
-    //                                .frame(width: 362, height: 95)
-    //                                .overlay(//테두리
-    //                                    RoundedRectangle(cornerRadius: 13)
-    //                                        .stroke(
-    //                                            Color.black.opacity(0.1),
-    //                                            lineWidth: 1
-    //                                        )
-    //                                )
-    //                            HStack {
-    //                                Image("Person")
-    //                                    .resizable()
-    //                                    .frame(width: 50, height: 50)
-    //                                VStack(alignment:.leading) {
-    //                                    Text("일반 사용자")
-    //                                        .foregroundColor(Color.black)
-    //                                        .font(.system(size: 17))
-    //                                        .fontWeight(.bold)
-    //                                        .padding(.bottom, 1)
-    //                                    Text("마트에서 똑똑하게 쇼핑하고 싶어요.")
-    //                                        .foregroundColor(Color(red:106/255, green: 114/255, blue: 130/255))
-    //                                        .font(.system(size: 14))
-    //                                        .fontWeight(.regular)
-    //                                }
-    //                                .padding(10)
-    //                                Spacer()
-    //                            }
-    //                            .padding(.horizontal, 50)
-                                
                             }//zstack
                             .onTapGesture {
                                 viewModel.selectNormalUser()
@@ -93,50 +64,10 @@ struct SelectView: View {
                         .frame(width: 362, height: 55, alignment: .center)
                         .background(Color(red: 0.25, green: 0.28, blue: 0.61))
                         .cornerRadius(12)
-                        
-                            
-                        }
-                        .padding(.bottom, 10)
-                        //제휴 파트너 버튼
-//                        ZStack {
-//                            RoundedRectangle(cornerRadius: 13)
-//                                .fill(Color.white)
-//                                .frame(width: 362, height: 95)
-//                                .overlay(//테두리
-//                                    RoundedRectangle(cornerRadius: 13)
-//                                        .stroke(
-//                                            Color.black.opacity(0.1),
-//                                            lineWidth: 1
-//                                        )
-//                                )
-//                            HStack {
-//                                Image("Store")
-//                                    .resizable()
-//                                    .frame(width: 50, height: 50)
-//                                VStack(alignment:.leading) {
-//                                    Text("제휴 파트너")
-//                                        .foregroundColor(Color.black)
-//                                        .font(.system(size: 17))
-//                                        .fontWeight(.bold)
-//                                        .padding(.bottom, 1)
-//                                    Text("우리 마트를 홍보하고 싶어요.")
-//                                        .foregroundColor(Color(red:106/255, green: 114/255, blue: 130/255))
-//                                        .font(.system(size: 14))
-//                                        .fontWeight(.regular)
-//                                }
-//                                .padding(10)
-//                                Spacer()
-//                            }
-//                            .padding(.horizontal, 50)
-//                        }//제휴파트너 버튼
-//                        .onTapGesture {
-//                            viewModel.selectPartner()
-//                            //appState.userType = .partner
-                        }
                     }//버튼 VStack
                     .padding(.top, 150)
-                    Spacer()
-                }
+                    //Spacer()
+                }//vstack
             }//zstack
             .navigationDestination(isPresented: $goToCamera) {
                 CameraOCRView()
