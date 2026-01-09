d168e30479423ac5ffe79567d2bbe824ecce237b
박진수 <jisnu@handong.ac.kr>
2026-01-09 18:13:05 +0900

    almost there

--- Shoppingmate_Frontend/View/OCRResultView/ProductCardView.swift
+++ Shoppingmate_Frontend/View/OCRResultView/ProductCardView.swift
@@ -35,8 +35,9 @@ struct ProductCardView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: side, height: side)
+//                                    .clipped()
                                     .background(
-                                        RoundedRectangle(cornerRadius: 9)
+                                        RoundedRectangle(cornerRadius: 9) 
                                             .fill(Color(red: 0.91, green: 0.91, blue: 0.91))
                                     )
 
@@ -68,12 +69,13 @@ struct ProductCardView: View {
                     )
                         .foregroundStyle(
                                 LinearGradient(
-                                    colors: [
-                                        Color(red: 0.65, green: 0.32, blue: 0.91),
-                                        Color(red: 0.19, green: 0.53, blue: 1)
+                                    stops: [
+                                        .init(color: Color(red: 0.65, green: 0.32, blue: 0.91), location: 0.00),
+                                        .init(color: Color(red: 0.19, green: 0.53, blue: 1.00), location: 0.53),
+                                        .init(color: Color(red: 0.04, green: 0.83, blue: 0.73), location: 1.00),
                                     ],
-                                    startPoint: .leading,
-                                    endPoint: .trailing
+                                    startPoint: UnitPoint(x: -0.01, y: 0.49),
+                                    endPoint: UnitPoint(x: 1.00, y: 0.49)
                                 )
                             )
                         .padding(.horizontal, 6)
@@ -87,12 +89,13 @@ struct ProductCardView: View {
                             //.inset(by: 0.65)
                                 .stroke(
                                     LinearGradient(
-                                        colors: [
-                                            Color(red: 0.65, green: 0.32, blue: 0.91),
-                                            Color(red: 0.19, green: 0.53, blue: 1)
+                                        stops: [
+                                        Gradient.Stop(color: Color(red: 0.65, green: 0.32, blue: 0.91), location: 0.00),
+                                        Gradient.Stop(color: Color(red: 0.19, green: 0.53, blue: 1), location: 0.53),
+                                        Gradient.Stop(color: Color(red: 0.04, green: 0.83, blue: 0.73), location: 1.00),
                                         ],
-                                        startPoint: .leading,
-                                        endPoint: .trailing
+                                        startPoint: UnitPoint(x: -0.01, y: 0.49),
+                                        endPoint: UnitPoint(x: 1, y: 0.49)
                                     ),
                                 lineWidth: 1.3)
 
@@ -121,28 +124,36 @@ struct ProductCardView: View {
                       .font(.custom("Pretendard-Bold", size: 10))
                       .foregroundColor(Color(red: 0.06, green: 0.09, blue: 0.16))
                     Spacer()
-                    if let price = Int(product.price) {
-                        Text("\(price.formatted(.number))원")
+                        let price = product.price.digitsInt
+                         Text(price.map { "\($0.formatted(.number))원" } ?? "-")
                             .font(.custom("Pretendard-Bold", size: 18))
                             .foregroundColor(Color(red: 0.06, green: 0.09, blue: 0.16))
                             .padding(.leading, 5)
                             .padding(.bottom, 2)
-                    }
                 }
                 HStack {
                     Text("온라인 최저가")
                         .font(.custom("Pretendard-Bold", size: 10))
                         .foregroundColor(Color(red: 0.06, green: 0.09, blue: 0.16))
                     Spacer()
-                    if let price = Int(product.onlinePrice) {
-                        Text("\(price.formatted(.number))원")
-                            .font(.custom("Pretendard-Bold", size: 18))
+                    let price = product.onlinePrice.digitsInt
+                    Text(price.map { "\($0.formatted(.number))원" } ?? "-")
+                    //                    if let price = Int(product.onlinePrice) {
+                    //                        Text("\(price.formatted(.number))원")
+                        .font(.custom("Pretendard-Bold", size: 18))
                             .foregroundColor(Color(red: 0.06, green: 0.09, blue: 0.16))
                             .padding(.leading, 5)
                             .padding(.bottom, 2)
-                    }
+//                    }
                 }
             } // VStack text
         } // VStack all
+    }
+}
+
+extension String {
+    var digitsInt: Int? {
+        let digits = self.filter { $0.isNumber }
+        return Int(digits)
     }
 }
